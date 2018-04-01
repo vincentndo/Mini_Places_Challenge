b55dec0b3718b5c837d10e8d0ee225c71588c8ba
SsnL <tongzhou.wang.1994@gmail.com>
2018-04-01 01:28:06 -0400

    test training

---
+++ dataset.py
@@ -0,0 +1,58 @@
+import os
+import glob
+import torch
+import torch.utils.data as data
+from torchvision import transforms
+from PIL import Image
+
+
+class MiniplacesDataset(data.Dataset):
+    num_classes = 100
+    mean = [0.45432566120701207, 0.4362440955619064, 0.40466656402468215]
+    std = [0.2643629213905866, 0.2612573977850761, 0.2790479352583138]
+
+    def __init__(self, split, crop_size, label_root='./labels',
+                 image_root='./images'):
+        assert split in ('train', 'val', 'test'), "split must be one of: train, val, and test"
+        image_dir = os.path.join(image_root, split)
+        self.split = split
+        # build transform
+        if split == 'train':
+            self.transform = transforms.Compose([
+                transforms.RandomCrop([crop_size, crop_size]),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(self.mean, self.std),
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.RandomCrop([crop_size, crop_size]),
+                transforms.ToTensor(),
+                transforms.Normalize(self.mean, self.std),
+            ])
+        # build image path list
+        if split == 'test':
+            self.images = glob.glob(os.path.join(image_dir, '*'))
+        else:
+            label_dir = os.path.join(label_root, split + '.txt')
+            with open(label_dir, 'r') as f:
+                self.images = []
+                self.labels = []
+                for line in f:
+                    image, label = line.strip().split()
+                    label = int(label)
+                    self.images.append(os.path.join(image_root, image))
+                    self.labels.append(label)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        path = self.images[idx]
+        with open(path, 'rb') as f:
+            img = Image.open(f).convert('RGB')
+        tensor = self.transform(img)
+        if self.split == 'test':
+            return tensor
+        else:
+            return tensor, self.labels[idx]
---
+++ models.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+
+
+class MiniAlexNet(nn.Module):
+    r"""
+    A variant of AlexNet.
+    The changes with respect to the original AlexNet are:
+        - LRN (local response normalization) layers are not included
+        - The Fully Connected (FC) layers (fc6 and fc7) have smaller dimensions
+          due to the lower resolution of mini-places images (128x128) compared
+          with ImageNet images (usually resized to 256x256)
+    """
+    def __init__(self):
+        super(MiniAlexNet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(96, 256, kernel_size=5, padding=2, groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1, groups=2),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1, groups=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(256, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(256, 100),
+        )
+
+    def forward(self, input):
+        features = self.conv(input)
+        features = features.view(input.size(0), -1)  # flatten to a 2d tensor
+        return self.fc(features)
